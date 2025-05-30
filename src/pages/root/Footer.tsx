a1ecb9968d9fce59e85dbe8a256230a289f638ca
majaluoma <olli.majaluoma@gmail.com>
2025-05-30 18:25:13 +0300

    removed wrong texts

--- src/pages/root/Footer.tsx
+++ src/pages/root/Footer.tsx
@@ -6,15 +6,10 @@ export default function Footer() {
   return (
     <footer className="z-1 bg-muted text-muted-foreground mt-auto w-full shadow-lg shadow-secondary overflow-hidden">
       <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center gap-4">
-        <h2 className="text-2xl font-bold text-primary">majaluoma.fi</h2>
+        <h2 className="text-2xl font-bold text-primary">Kerava cup</h2>
 
         <p className="flex flex-row align-baseline">
-          Sähköposti:{' '}
-          <img
-            src={'/assets/contactEmailImage.png'}
-            alt="sahkopostiosoite"
-            className="align-baseline ml-2 w-auto h-max relative "
-          />
+         
         </p>
       <p>
         &copy; {new Date().getFullYear()} Majaluoma.fi All rights
